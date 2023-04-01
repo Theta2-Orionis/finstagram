a9f3207483c98e9c868393ce7f2973e1fa450a92
Graham Christensen <graham@drumhellerrocks.com>
2023-04-01 21:49:14 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,7 +1,13 @@
+helpers do
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
-end
+  end
 
 get '/signup' do
     @user = User.new
@@ -9,16 +15,44 @@ get '/signup' do
 end
 
 post '/signup' do
-    email = params[:email]
+    email      = params[:email]
     avatar_url = params[:avatar_url]
+    username   = params[:username]
+    password   = params[:password]
+  
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+  
+    if @user.save
+      redirect to('/login')
+    else
+      erb(:signup)
+    end
+end
+
+get '/laramidia' do
+    erb(:laramidia)
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
     username = params[:username]
     password = params[:password]
-
-    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
-    
-    if @user.save
-        "User #{username} saved!"
+  
+    user = User.find_by(username: username)  
+  
+    if user && user.password == password
+      session[:user_id] = user.id
+      redirect to('/')
     else
-        erb(:signup)
+      @error_message = "Login failed."
+      erb(:login)
     end
+  end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
 end
